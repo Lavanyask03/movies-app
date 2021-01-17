ef14633047a91f1bf37012b4c1eded2761a7c9a3
Lavanya <lavanyask03@gmail.com>
2021-01-17 17:50:44 +0530

    adding movie plot

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Movie.css';
-
+/* 
 const Movie = (props) => {
 
     const POSTER_UNAVAILABLE = "https://media.comicbook.com/files/img/default-movie.png?auto=webp"
@@ -17,6 +17,61 @@ const Movie = (props) => {
            </div>
         </div>
     );
+}
+ */
+
+class Movie extends Component {
+
+    API_KEY = "bad70be0";
+
+    state = {
+        movie_plot: ''
+    }
+
+    componentDidMount() {
+        let title = this.props.Title;
+        this.setPlot(title);
+    }
+
+    setPlot = (title) => {
+        if (title.trim()) {
+            const url = `https://www.omdbapi.com/?t=${title}&apikey=${this.API_KEY}&plot=short`;
+           fetch(url)
+            .then(response => response.json())
+            .then(jsonResponse => {
+                this.setState({movie_plot:jsonResponse.Plot});
+            });     
+        } 
+        else {
+          return;      
+        }
+    }
+
+    render() {
+        const POSTER_UNAVAILABLE = "https://media.comicbook.com/files/img/default-movie.png?auto=webp"
+
+        const {Title, Year, Poster} = this.props;
+
+        const PosterURL = Poster === "N/A"? POSTER_UNAVAILABLE :Poster;
+
+        return (
+            <div className = 'movie flip-card' >
+                <div className="flip-card-inner">
+                    <div className="flip-card-front">
+                        <img className="poster" src={PosterURL} alt="my movie poster" width="400" />
+                        <div className="title-year">
+                            <h1 className="title"> {Title} </h1>
+                            <h2 className="year"> {Year} </h2>
+                        </div>
+                    </div>
+                
+                    <div className="plot flip-card-back">
+                        {this.state.movie_plot}
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Movie;
